85ec6ba33c6af0ba85d33a2bf400e95f30c42289
Christopher.Chlebowski <s80456@informatik.htw-dresden.de>
2024-12-07 12:17:01 +0100

    feat: added additional gridlines, depending on current zoomLevel

--- ProofOfConcept_Timeline/src/components/Grid.vue
+++ ProofOfConcept_Timeline/src/components/Grid.vue
@@ -26,23 +26,37 @@ export default defineComponent({
     });
     function renderGrid() {
       gridContainer = new Pixi.Container();
-      for (let second = 0; second < pixiApp.canvas.width / (config.pixelsPerSecond * store.state.zoomLevel); second++) {
-        const y = props.trackCount * config.trackHeight + config.componentPadding + config.sliderHeight;
-        const x = second * config.pixelsPerSecond * store.state.zoomLevel;
+      const pixelPerSeconds = config.pixelsPerSecond * store.state.zoomLevel;
+      const steps = (pixiApp.canvas.width / pixelPerSeconds);
+      
+      let intervals = [1]; // standard: 1-second-spacing
+      if (store.state.zoomLevel >= config.maxZoom / 3) intervals.push(0.5); // half
+      if (store.state.zoomLevel >= config.maxZoom / 2) intervals.push(0.25); // quarter
+      
+      intervals.forEach((interval) => {
+        const isMajor = interval == 1;
+        const lineWidth = isMajor ? 2 : 1;
         
-        gridGraphics.moveTo(x, config.sliderHeight + config.componentPadding);
-        gridGraphics.lineTo(x, y);
-        gridGraphics.stroke({width: 1, color: config.colors.gridColor})
+        for (let step = 0; step < steps/interval; step++) {
+          const y = props.trackCount * config.trackHeight + config.componentPadding + config.sliderHeight;
+          const x = step * pixelPerSeconds * interval;
 
-        const label = new Pixi.Text();
-        label.text = second;
-        label.style.fontSize = 12;
-        label.x = x - (label.width / 2);  
-        label.y = y;
-        
-        gridContainer.addChild(label);     
-      }
-
+          gridGraphics.moveTo(x, config.sliderHeight + config.componentPadding);
+          gridGraphics.lineTo(x, y);
+          gridGraphics.stroke({width: lineWidth, color: config.colors.gridColor})
+    
+          if (isMajor) {
+            const label = new Pixi.Text();
+            label.text = step;
+            label.style.fontSize = 12;
+            label.x = x - (label.width / 2);
+            label.y = y;
+            
+            gridContainer!.addChild(label);
+          }
+        }
+      });      
+      
       gridContainer.addChild(gridGraphics);
       pixiApp.stage.addChild(gridContainer);
       gridContainer.x = 48;

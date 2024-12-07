24e4de55391b509e5b13011e66417980ccef8fa8
Christopher.Chlebowski <s80456@informatik.htw-dresden.de>
2024-12-07 12:19:29 +0100

    chore: updated config values
    - increased maxZoomLevel
    - decreased minSliderWidth
    - hide resizingHandles for tactons (visible only for debugging purpose)

--- ProofOfConcept_Timeline/src/config.ts
+++ ProofOfConcept_Timeline/src/config.ts
@@ -3,9 +3,9 @@ const config = {
     componentPadding: 12,
     // slider
     sliderHeight: 28,
-    sliderMinWidth: 150,
+    sliderMinWidth: 50,
     sliderHandleWidth: 32,
-    maxZoom: 10,
+    maxZoom: 20,
     minZoom: 1,
     // track
     trackHeight: 150,
@@ -16,7 +16,7 @@ const config = {
     pixelsPerSecond: 100,
     colors: {
         gridColor: 'rgba(75, 75, 75, 0.2)',
-        handleColor: 'rgba(236,102,12,0.2)',
+        handleColor: 'rgba(236,102,12,0)',
         tactonColor: '#848484',
         trackLineColor: '#777777',
         sliderHandleColor: 'rgba(236,102,12,1)'

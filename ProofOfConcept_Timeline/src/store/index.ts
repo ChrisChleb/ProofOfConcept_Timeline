66f33f6b72ce44b5cd0e741f3ce6a7a3f27a66c2
Christopher.Chlebowski <s80456@informatik.htw-dresden.de>
2025-01-08 12:38:48 +0100

    feat: store tactons at store
    - all tactons are now stored centrally in the vue-store instead of in the individual track components
    - added function for sorting tactons, based on der x-position
    --> this way, the last tacton of the sequence can be obtained by accessing the last element of the array, making the calculation of the sequence length more efficient
    - added (initial/current)virtualViewportWidth to the store
    --> stores, how long the sequence is / initial was
    - added sliderOffset to the store
    --> the offset, by which every element is shifted is now split into two parts
    --> sliderOffset: the offset applied by dragging the slider
    --> viewportOffset: the offset applied when tacton is moved out of the currentViewport, thus increasing the virtualViewportWidth

--- ProofOfConcept_Timeline/src/store/index.ts
+++ ProofOfConcept_Timeline/src/store/index.ts
@@ -1,10 +1,18 @@
 import {createStore} from 'vuex';
+import type {TactonDTO} from "@/components/Track.vue";
+import pixiApp from "@/pixi/pixiApp";
+import * as Pixi from "pixi.js";
 
 const store = createStore({
     state: {
         zoomLevel: 1,
+        sliderOffset: 0,
         viewportOffset: 0,
-        gridLines: [] as number[]
+        gridLines: [] as number[],  
+        sorted: {} as Record<number, boolean>,
+        tactons: {} as Record<number, TactonDTO[]>,        
+        initialVirtualViewportWidth: 0,
+        currentVirtualViewportWidth: 0,
     },
     mutations: {
         setZoomLevel(state: any, zoomLevel: number): void {
@@ -13,8 +21,57 @@ const store = createStore({
         setViewportOffset(state: any, viewportOffset: number): void {
             state.viewportOffset = viewportOffset;
         },
+        setSliderOffset(state: any, newSliderOffset: number): void {
+          state.sliderOffset = newSliderOffset;  
+        },
         setGridLines(state: any, gridLines: []): void {
             state.gridLines = gridLines;
+        },
+        addTacton(state: any, {trackId, newTacton}: {trackId: number, newTacton: TactonDTO}): void {
+            if (!state.tactons[trackId]) {
+                state.tactons[trackId] = [];
+                state.sorted[trackId] = false;
+            }
+            state.tactons[trackId].push(newTacton);
+        },
+        sortTactons(state: any): void {
+            const sortedTactons: Record<number, TactonDTO[]> = {};                        
+            Object.keys(state.tactons).forEach((channel: string, trackId: number): void => {                
+                if (state.sorted[trackId]) {
+                    sortedTactons[trackId] = state.tactons[trackId];
+                } else {
+                    sortedTactons[trackId] = [...state.tactons[trackId]].sort((a: TactonDTO, b: TactonDTO): number => {
+                        const rectA: Pixi.Graphics = a.rect;
+                        const rectB: Pixi.Graphics = b.rect;
+                        if (rectA.x === rectB.x) {
+                            return rectB.width - rectA.width;
+                        }
+                        return rectA.x - b.rect.x;
+                    });
+                    state.sorted[trackId] = true;                  
+                }
+            });
+            
+            state.tactons = sortedTactons;
+        },
+        deleteTactons(state: any, trackId: number): void {
+            if (state.tactons[trackId] == undefined) return;
+            state.tactons[trackId].forEach((dto: TactonDTO): void => {
+                pixiApp.stage.removeChild(dto.container);
+                dto.container.children.forEach((child: Pixi.ContainerChild): void => {
+                    child.removeAllListeners();
+                });
+                dto.container.removeAllListeners();
+                dto.container.destroy({children: true});
+            });
+
+            delete state.tactons[trackId];
+        },
+        setInitialVirtualViewportWidth(state: any, newWidth: number): void {
+            state.initialVirtualViewportWidth = newWidth;
+        },
+        setCurrentVirtualViewportWidth(state: any, newWidth: number): void {
+            state.currentVirtualViewportWidth = newWidth;
         }
     },
     actions: {
@@ -24,14 +81,37 @@ const store = createStore({
         updateViewportOffset({ commit }: any, newViewportOffset: number): void {
             commit('setViewportOffset', newViewportOffset);
         },
+        updateSliderOffset({ commit }: any, newSliderOffset: number): void {
+          commit('setSliderOffset', newSliderOffset);  
+        },
         updateGridLines({ commit }: any, newGridLines: []): void {
             commit('setGridLines', newGridLines);
-        }
+        },
+        addTacton({ commit }: any, {trackId, newTacton}: {trackId: number, newTacton: TactonDTO}): void {
+            commit('addTacton', {trackId, newTacton});
+        },
+        sortTactons({ commit }: any): void {
+            commit('sortTactons');
+        },
+        deleteTactons({ commit }: any, trackId: number): void {
+            commit('deleteTactons', trackId);
+        },
+        updateInitialVirtualViewportWidth({ commit }: any, newWidth: number): void {
+            commit('setInitialVirtualViewportWidth', newWidth);
+        },
+        updateCurrentVirtualViewportWidth({ commit }: any, newWidth: number): void {
+            commit('setCurrentVirtualViewportWidth', newWidth);
+        },
     },
     getters: {
         zoomLevel: (state: any) => state.zoomLevel,
         viewportOffset: (state: any) => state.viewportOffset,
-        gridLines: (state: any) => state.gridLines
+        sliderOffset: (state: any) => state.sliderOffset,
+        gridLines: (state: any) => state.gridLines,
+        tactons: (state: any) => state.tactons,
+        initialVirtualViewportWidth: (state: any) => state.initialVirtualViewportWidth,
+        currentVirtualViewportWidth: (state: any) => state.currentVirtualViewportWidth,
+        sorted: (state: any) => state.sorted
     }
 });
 export default store;

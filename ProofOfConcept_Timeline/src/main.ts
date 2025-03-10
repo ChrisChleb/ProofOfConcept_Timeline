50f894866b9439e1bf2fbfc2ef0de836fc2947dc
Christopher.Chlebowski <s80456@informatik.htw-dresden.de>
2025-03-10 12:15:17 +0100

    chore: added vuetify

--- ProofOfConcept_Timeline/src/main.ts
+++ ProofOfConcept_Timeline/src/main.ts
@@ -5,9 +5,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-const app = createApp(App)
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
+const app = createApp(App);
 
 app.use(store);
 app.use(router);
+app.use(vuetify);
 
 app.mount('#app');
